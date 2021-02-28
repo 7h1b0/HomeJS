d2f37b0e817a8da36933476c181753d62cffa436
7h1b0 <7h1b0@users.noreply.github.com>
2021-02-28 20:26:39 +0100

    wip

--- packages/anna-web/src/components/title.tsx
+++ packages/anna-web/src/components/title.tsx
@@ -27,9 +27,9 @@ function Title({ title, subtitle, activateNavigation = false, action }: Props) {
             className="text-white fill-current w-5"
           >
             <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
               d="M10 19l-7-7m0 0l7-7m-7 7h18"
             />
           </svg>
